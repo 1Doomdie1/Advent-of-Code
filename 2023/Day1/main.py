5e5e9fb02364e7a33b4b304eb0398631c587853a
1Doomdie1 <horia.todoran@yahoo.com>
2023-12-05 20:54:02 +0200

    Day 1 solution added.

---
+++ 2023/Day1/main.py
@@ -0,0 +1,35 @@
+def get_data():
+    with open("data.txt", "r") as file:
+        return file.read().split("\n")
+
+def day_one():
+    data = get_data()
+    total = 0
+    for line in data:
+        digits = [letter for letter in line if letter.isdigit()]
+        if len(digits) > 1:
+            total += int(f"{digits[0]}{digits[-1]}")
+        elif len(digits) == 1:
+            total += int(f"{''.join(digits * 2)}")
+    print(total)
+
+def day_two():
+    data = get_data()
+    word_to_digit = {
+        'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5',
+        'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
+    }
+    total = 0
+    for line in data:
+        for word, digit in word_to_digit.items():
+            line = line.replace(word, f"{word}{digit}{word}")
+        digits = [letter for letter in line if letter.isdigit()]
+        if len(digits) > 1:
+            total += int(f"{digits[0]}{digits[-1]}")
+        elif len(digits) == 1:
+            total += int(f"{''.join(digits * 2)}")
+    print(total)
+
+if __name__ == "__main__":
+    day_one()
+    day_two()
